2aa5b0d2ac8cc1c4196de4ad6ce9bf084ac5302c
KarthikKollapureddy <karthiksaik2000@gmail.com>
2024-08-29 18:38:07 +0530

    Add @BeforeEach and @AfterEach

--- src/test/java/org/pratice/DemoUtilsTest.java
+++ src/test/java/org/pratice/DemoUtilsTest.java
@@ -1,26 +1,37 @@
 package org.pratice;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //no need to specify class as public
 class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+//    Executes before each test (@Test)
+    @BeforeEach
+    void initialSetup(){
+        demoUtils = new DemoUtils();
+    }
+
+    @AfterEach
+    void releaseResources(){
+//        System.out.println("Executes after each test case, mostly to release resources and for cleanup");
+    }
     @Test
     void test_Add_Method(){
-        DemoUtils demoUtils = new DemoUtils();
         assertEquals(8,demoUtils.add(2,6),"2+6 should equals to 8");
         assertNotEquals(6,demoUtils.add(2,6),"2+6 should not equals to 6");
     }
     @Test
     void test_String_is_Null_OR_Empty(){
-        DemoUtils demoUtils = new DemoUtils();
         assertTrue(demoUtils.isNull("      "),"String is null/empty");
         assertFalse(demoUtils.isNull("Hello"),"String Hello is not null/empty");
     }
     @Test
     void Test_inputString(){
-        DemoUtils demoUtils = new DemoUtils();
         assertNull(demoUtils.returnInputString(null),"Input message is Null");
         assertNotNull(demoUtils.returnInputString("Not Null"),"Input message is not Null");
     }
