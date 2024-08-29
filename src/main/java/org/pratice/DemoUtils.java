6cb37742e2bbc6ed2f6b1511d44d48c7a7fe2745
KarthikKollapureddy <karthiksaik2000@gmail.com>
2024-08-29 18:33:25 +0530

    create basic test cases

--- src/main/java/org/pratice/DemoUtils.java
+++ src/main/java/org/pratice/DemoUtils.java
@@ -4,4 +4,13 @@ public class DemoUtils {
     public int add(int a, int b){
         return a+b;
     }
+
+    public boolean isNull(String str){
+        str = str.strip();
+        str = str.stripTrailing();
+        return str.isEmpty();
+    }
+    public String returnInputString(String str){
+        return str;
+    }
 }
--- src/test/java/org/pratice/DemoUtilsTest.java
+++ src/test/java/org/pratice/DemoUtilsTest.java
@@ -2,8 +2,7 @@ package org.pratice;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 //no need to specify class as public
 class DemoUtilsTest {
@@ -12,5 +11,17 @@ class DemoUtilsTest {
         DemoUtils demoUtils = new DemoUtils();
         assertEquals(8,demoUtils.add(2,6),"2+6 should equals to 8");
         assertNotEquals(6,demoUtils.add(2,6),"2+6 should not equals to 6");
+    }
+    @Test
+    void test_String_is_Null_OR_Empty(){
+        DemoUtils demoUtils = new DemoUtils();
+        assertTrue(demoUtils.isNull("      "),"String is null/empty");
+        assertFalse(demoUtils.isNull("Hello"),"String Hello is not null/empty");
+    }
+    @Test
+    void Test_inputString(){
+        DemoUtils demoUtils = new DemoUtils();
+        assertNull(demoUtils.returnInputString(null),"Input message is Null");
+        assertNotNull(demoUtils.returnInputString("Not Null"),"Input message is not Null");
     }
 }
