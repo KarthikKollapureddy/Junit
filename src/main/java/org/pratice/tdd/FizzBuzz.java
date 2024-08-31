8528e74f803d061e36709ea1aa2b29d387af0859
KarthikKollapureddy <karthiksasik2000@ggmail.com>
2024-08-31 20:47:31 +0530

    create implementation for test.

--- src/main/java/org/pratice/tdd/FizzBuzz.java
+++ src/main/java/org/pratice/tdd/FizzBuzz.java
@@ -2,7 +2,12 @@ package org.pratice.tdd;
 
 public class FizzBuzz {
     public static String compute(int i) {
-        //yet to impl
-        return null;
+        if(i%5 == 0 && i%3 == 0){
+            return "FizzBuzz";
+        } else if (i%3 == 0) {
+            return "Fizz";
+        } else if (i%5 == 0) {
+            return  "Buzz";
+        }return Integer.toString(i);
     }
 }
