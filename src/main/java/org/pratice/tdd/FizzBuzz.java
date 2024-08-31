7c1d10b3463daf10186c625ae2131df6d5b44bac
KarthikKollapureddy <karthiksasik2000@ggmail.com>
2024-08-31 21:04:08 +0530

    Refactored FizzBuzz Implementation.

--- src/main/java/org/pratice/tdd/FizzBuzz.java
+++ src/main/java/org/pratice/tdd/FizzBuzz.java
@@ -2,12 +2,20 @@ package org.pratice.tdd;
 
 public class FizzBuzz {
     public static String compute(int i) {
-        if(i%5 == 0 && i%3 == 0){
-            return "FizzBuzz";
-        } else if (i%3 == 0) {
-            return "Fizz";
-        } else if (i%5 == 0) {
-            return  "Buzz";
-        }return Integer.toString(i);
+//        if(i%5 == 0 && i%3 == 0){
+//            return "FizzBuzz";
+//        } else if (i%3 == 0) {
+//            return "Fizz";
+//        } else if (i%5 == 0) {
+//            return  "Buzz";
+//        }return Integer.toString(i);
+        StringBuilder result = new StringBuilder();
+        if(i%3 == 0)
+            result.append("Fizz");
+        if(i%5 == 0)
+            result.append("Buzz");
+        if(result.isEmpty())
+            result.append(i);
+        return result.toString();
     }
 }
