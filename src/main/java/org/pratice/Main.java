276c30b1b3078d41538a0a87356931142b4e00e3
KarthikKollapureddy <karthiksasik2000@ggmail.com>
2024-08-31 22:13:08 +0530

    Create main app for FizzBuzz.

--- src/main/java/org/pratice/Main.java
+++ src/main/java/org/pratice/Main.java
@@ -1,7 +1,13 @@
 package org.pratice;
 
+import org.pratice.tdd.FizzBuzz;
+
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IntStream.range(0,101).mapToObj(
+                FizzBuzz::compute
+        ).forEach(System.out::println);
     }
 }
